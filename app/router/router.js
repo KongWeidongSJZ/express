5567ede37b020108f81ed043a465f05263a5fc8e
kongweidong <terry_kong@sinobasedm.com>
2018-11-09 17:15:18 +0800

    初始化

--- app/router/router.js
+++ app/router/router.js
@@ -2,20 +2,20 @@ const router = require('koa-router')();
 router.post('/member/register', async (ctx) => {
     function getInfo() {
         return new Promise(function (res, rej) {
-            setTimeout(function(){
+            setTimeout(function () {
                 res({
                     result: 0,
                     content: {
                         a: 1
                     }
                 })
-            },1000)
+            }, 1000)
         });
     }
     ctx.body = await getInfo();
 });
 router.get('/', function (ctx) {
-    ctx.body='hello world'
+    ctx.body = 'hello world'
 });
 
 module.exports = router;
